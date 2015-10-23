a93784127c0db5ca72d68f2863c5abcc7bfbbd7d
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2015-10-23 15:27:59 +0300

    Update views.py

--- shelters/views.py
+++ shelters/views.py
@@ -30,7 +30,7 @@ def index(request):
     }, RequestContext(request))
 
 def list(request):
-    shelters = models.Shelter.objects.order_by('occupants', 'type', 'shelter_idgit ')
+    shelters = models.Shelter.objects.order_by('occupants', 'type', 'shelter_id ')
     return render_to_response("list.html", {
         "shelters": shelters,
     }, RequestContext(request))
